19351c11f96857f9324055c13ddca8a04349cf38
chrizsenpaii <134850628+chrizsenpaii@users.noreply.github.com>
2024-07-20 13:08:05 +0800

    Add files via upload

---
+++ test.js
@@ -0,0 +1,13 @@
+function HouseKeeper(name, yearsOFExperience, cleaningRepertoire){
+    this.name = name;
+    this.yearsOFExperience = yearsOFExperience;
+    this.cleaningRepertoire = cleaningRepertoire;
+}
+const houseKeeper1 = new HouseKeeper("Christian",2,["cleaning", "sht"])
+document.addEventListener("keypress", function (event) {
+    
+})
+
+function makeSound(key){
+    
+}
